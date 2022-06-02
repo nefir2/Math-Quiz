3603c60f13070abf78624ac26fc164ac3d124a9e
nefir2 <vadim.ahtamov@gmail.com>
2022-06-02 15:06:36 +0300

    All is done.

--- Math Quiz/Form2.Designer.cs
+++ Math Quiz/Form2.Designer.cs
@@ -29,26 +29,40 @@ namespace Math_Quiz
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.components = new System.ComponentModel.Container();
+            this.changeTypeButton = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.timelabel = new System.Windows.Forms.Label();
             this.startButton = new System.Windows.Forms.Button();
+            this.firstWord = new System.Windows.Forms.Label();
+            this.secondWord = new System.Windows.Forms.Label();
+            this.thirdWord = new System.Windows.Forms.Label();
+            this.fourthWord = new System.Windows.Forms.Label();
+            this.fifthWord = new System.Windows.Forms.Label();
+            this.firstBox = new System.Windows.Forms.TextBox();
+            this.secondBox = new System.Windows.Forms.TextBox();
+            this.thirdBox = new System.Windows.Forms.TextBox();
+            this.fourthBox = new System.Windows.Forms.TextBox();
+            this.fifthBox = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // button1
+            // changeTypeButton
             // 
-            this.button1.AutoSize = true;
-            this.button1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.button1.BackColor = System.Drawing.Color.MediumBlue;
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button1.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.button1.Location = new System.Drawing.Point(617, 407);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(190, 34);
-            this.button1.TabIndex = 34;
-            this.button1.Text = "сменить тип теста";
-            this.button1.UseVisualStyleBackColor = false;
-            this.button1.Click += new System.EventHandler(this.ChangeType);
+            this.changeTypeButton.AutoSize = true;
+            this.changeTypeButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.changeTypeButton.BackColor = System.Drawing.Color.MediumBlue;
+            this.changeTypeButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.changeTypeButton.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.changeTypeButton.Location = new System.Drawing.Point(617, 407);
+            this.changeTypeButton.Name = "changeTypeButton";
+            this.changeTypeButton.Size = new System.Drawing.Size(190, 34);
+            this.changeTypeButton.TabIndex = 34;
+            this.changeTypeButton.TabStop = false;
+            this.changeTypeButton.Text = "сменить тип теста";
+            this.changeTypeButton.UseVisualStyleBackColor = false;
+            this.changeTypeButton.Click += new System.EventHandler(this.ChangeType);
             // 
             // label1
             // 
@@ -81,16 +95,133 @@ namespace Math_Quiz
             this.startButton.TabIndex = 0;
             this.startButton.Text = "начать тест";
             this.startButton.UseVisualStyleBackColor = false;
+            this.startButton.Click += new System.EventHandler(this.ClickStart);
+            // 
+            // firstWord
+            // 
+            this.firstWord.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.firstWord.Location = new System.Drawing.Point(80, 73);
+            this.firstWord.Name = "firstWord";
+            this.firstWord.Size = new System.Drawing.Size(171, 50);
+            this.firstWord.TabIndex = 37;
+            this.firstWord.Text = "____________";
+            this.firstWord.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // secondWord
+            // 
+            this.secondWord.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.secondWord.Location = new System.Drawing.Point(80, 123);
+            this.secondWord.Name = "secondWord";
+            this.secondWord.Size = new System.Drawing.Size(171, 50);
+            this.secondWord.TabIndex = 38;
+            this.secondWord.Text = "____________";
+            this.secondWord.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // thirdWord
+            // 
+            this.thirdWord.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.thirdWord.Location = new System.Drawing.Point(80, 173);
+            this.thirdWord.Name = "thirdWord";
+            this.thirdWord.Size = new System.Drawing.Size(171, 50);
+            this.thirdWord.TabIndex = 39;
+            this.thirdWord.Text = "____________";
+            this.thirdWord.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // fourthWord
+            // 
+            this.fourthWord.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.fourthWord.Location = new System.Drawing.Point(80, 223);
+            this.fourthWord.Name = "fourthWord";
+            this.fourthWord.Size = new System.Drawing.Size(171, 50);
+            this.fourthWord.TabIndex = 40;
+            this.fourthWord.Text = "____________";
+            this.fourthWord.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // fifthWord
+            // 
+            this.fifthWord.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.fifthWord.Location = new System.Drawing.Point(80, 273);
+            this.fifthWord.Name = "fifthWord";
+            this.fifthWord.Size = new System.Drawing.Size(171, 50);
+            this.fifthWord.TabIndex = 41;
+            this.fifthWord.Text = "____________";
+            this.fifthWord.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // firstBox
+            // 
+            this.firstBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.firstBox.Location = new System.Drawing.Point(603, 81);
+            this.firstBox.Name = "firstBox";
+            this.firstBox.Size = new System.Drawing.Size(200, 35);
+            this.firstBox.TabIndex = 1;
+            // 
+            // secondBox
+            // 
+            this.secondBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.secondBox.Location = new System.Drawing.Point(603, 131);
+            this.secondBox.Name = "secondBox";
+            this.secondBox.Size = new System.Drawing.Size(200, 35);
+            this.secondBox.TabIndex = 2;
+            // 
+            // thirdBox
+            // 
+            this.thirdBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.thirdBox.Location = new System.Drawing.Point(603, 188);
+            this.thirdBox.Name = "thirdBox";
+            this.thirdBox.Size = new System.Drawing.Size(200, 35);
+            this.thirdBox.TabIndex = 3;
+            // 
+            // fourthBox
+            // 
+            this.fourthBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.fourthBox.Location = new System.Drawing.Point(603, 238);
+            this.fourthBox.Name = "fourthBox";
+            this.fourthBox.Size = new System.Drawing.Size(200, 35);
+            this.fourthBox.TabIndex = 4;
+            // 
+            // fifthBox
+            // 
+            this.fifthBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.fifthBox.Location = new System.Drawing.Point(603, 288);
+            this.fifthBox.Name = "fifthBox";
+            this.fifthBox.Size = new System.Drawing.Size(200, 35);
+            this.fifthBox.TabIndex = 5;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(605, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(202, 18);
+            this.label2.TabIndex = 42;
+            this.label2.Text = "* вводите слова полностью";
             // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(815, 449);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.fifthBox);
+            this.Controls.Add(this.fourthBox);
+            this.Controls.Add(this.thirdBox);
+            this.Controls.Add(this.secondBox);
+            this.Controls.Add(this.firstBox);
+            this.Controls.Add(this.fifthWord);
+            this.Controls.Add(this.fourthWord);
+            this.Controls.Add(this.thirdWord);
+            this.Controls.Add(this.secondWord);
+            this.Controls.Add(this.firstWord);
             this.Controls.Add(this.startButton);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.timelabel);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.changeTypeButton);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.MaximizeBox = false;
             this.Name = "Form2";
@@ -104,9 +235,21 @@ namespace Math_Quiz
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button changeTypeButton;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label timelabel;
         private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Label firstWord;
+        private System.Windows.Forms.Label secondWord;
+        private System.Windows.Forms.Label thirdWord;
+        private System.Windows.Forms.Label fourthWord;
+        private System.Windows.Forms.Label fifthWord;
+        private System.Windows.Forms.TextBox firstBox;
+        private System.Windows.Forms.TextBox secondBox;
+        private System.Windows.Forms.TextBox thirdBox;
+        private System.Windows.Forms.TextBox fourthBox;
+        private System.Windows.Forms.TextBox fifthBox;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
     }
 }
--- Math Quiz/Form2.cs
+++ Math Quiz/Form2.cs
@@ -26,11 +26,15 @@ namespace Math_Quiz
 		/// <summary>
 		/// массив со словами для теста.
 		/// </summary>
-		string[] words = 
+		static List<string> ALLwords = new List<string>() 
 		{ 
 			"работа", "коллекция", "торт", "винегрет", "жёсткий", 
-			"значок", "инцидент", "новичок", "приукрасить", "престижный" 
+			"значок", "инцидент", "новичок", "приукрасить", "престижный"
 		};
+		/// <summary>
+		/// коллекция в которой можно удалять слова без потери слов.
+		/// </summary>
+		List<string> words;
 
 		/// <summary>
 		/// массив букв которые можно заменить в словах.
@@ -41,50 +45,189 @@ namespace Math_Quiz
 		/// объект рандом для случайных чисел.
 		/// </summary>
 		Random rnd = new Random();
-		
+
+		/// <summary>
+		/// первое слово.
+		/// </summary>
+		string first;
+		/// <summary>
+		/// второе слово.
+		/// </summary>
+		string second;
+		/// <summary>
+		/// третье слово.
+		/// </summary>
+		string third;
+		/// <summary>
+		/// четвёртое слово.
+		/// </summary>
+		string fourth;
+		/// <summary>
+		/// пятое слово.
+		/// </summary>
+		string fifth;
+
+		/// <summary>
+		/// оставшееся время.
+		/// </summary>
+		/// <remarks>
+		/// поле, где хранится число оставшегося времени.
+		/// </remarks>
+		int timeleft;
+
 		//методы
 		/// <summary>
-		/// установка случайного слова с пропуском в лейбл в форме.
+		/// случайное слово.
 		/// </summary>
-		/// <param name="location">лейбл в который надо вставить случайное слово.</param>
-		private void SetProblem(Label location)
-		{
-			string word = words[rnd.Next(words.Length)]; //получение случайного слова из массива слов.
-			string transformatedWord = SetLoose(word);
-			location.Text = transformatedWord;
+		/// <remarks>
+		/// метод берёт случайное слово из коллекции, удаляет его оттуда, и возвращает.
+		/// </remarks>
+		/// <returns>случайное слово из коллекции тип <see cref="string"/></returns>
+		private string GetWord()
+        {
+			string word = words[rnd.Next(words.Count)]; //получение случайного слова из массива слов.
+			words.Remove(word);
+			return word;
 		}
 		/// <summary>
-		/// установка одного пропуска за место случайной гласной.
+		/// установка в слове одного пропуска за место случайной гласной.
 		/// </summary>
 		/// <param name="word">слово, в котором нужно сделать пропуск. </param>
 		/// <returns>слово с пропуском типа <see cref="string"/>.</returns>
 		private string SetLoose(string word)
 		{
-			StringBuilder worder = new StringBuilder(word);
-			bool repeat = true;
-			do
-			{
+			StringBuilder worder = new StringBuilder(word); //из-за того что string не разрешает изменять букву в слове
+															//используется класс StringBuilder
+			bool repeat = true; //если не установится ни одного пропуска,
+			do					//то нужно снова пройтись по буквам слова
+			{					//и попытаться установить пропуск.
 				for (int i = 0; i < worder.Length; i++)
-				{
+				{ //цикл букв слова.
 					for (int j = 0; j < заменяемыеБуквы.Length; j++)
-					{
-						if (worder[i] == заменяемыеБуквы[j] && rnd.Next(2) == 1)
-						{
-							worder[i] = '_';
-							repeat = false;
-							break;
+					{ //цикл разрешённых для замены букв.
+						if (worder[i] == заменяемыеБуквы[j] && i != worder.Length - 1 && rnd.Next(2) == 1)
+						{ //если буква совпадает с разрешённой буквой, не является последней, и случайная цифра (из 0 или 1) равна единице:
+							//устанавливается за место буквы пропуск
+							worder[i] = '_'; //для этого действия и нужен был StringBuilder.
+							repeat = false; //выход из цикла do { } while () и внешнего for () { }
+							break; //выход из этого цикла for () { }
 						}
 					}
-					if (repeat) break;
+					if (!repeat) break; //выход из внешнего цикла for () { }
 				}
-			} while (repeat);
-			return $"{worder}";
+			} while (repeat); //выход из цикла do { } while ()
+			return $"{worder}"; //возврат слова с пропуском
 		}
 
+		/// <summary>
+		/// ответ для проверки всех ответов.
+		/// </summary>
+		/// <returns>
+		/// если все слова будут верными, то возвращается <see langword="true"/>,<br/>
+		/// но если хотя бы одно слово будет неверным, то возвращается <see langword="false"/>.
+		/// </returns>
+		private bool CheckAns()
+		{
+			if (firstBox.Text  == first   &&
+				secondBox.Text == second  &&
+				thirdBox.Text  == third   &&
+				fourthBox.Text == fourth  &&
+				fifthBox.Text  == fifth
+				) return true;
+			else return false;
+		}
+
+		/// <summary>
+		/// начало теста.
+		/// </summary>
+		private void StartLT()
+        {
+			first = GetWord();
+			firstWord.Text = SetLoose(first); //установка слова с пропуском слова с пропуском.
+			firstBox.Text = ""; //очистка поля при начале
+
+			second = GetWord();
+			secondWord.Text = SetLoose(second);
+			secondBox.Text = "";
+
+			third = GetWord();
+			thirdWord.Text = SetLoose(third);
+			thirdBox.Text = "";
+
+			fourth = GetWord();
+			fourthWord.Text = SetLoose(fourth);
+			fourthBox.Text = "";
+
+			fifth = GetWord();
+			fifthWord.Text = SetLoose(fifth);
+			fifthBox.Text = "";
 
 
+			//запуск таймера при запуске теста.
+			timeleft = 30; 
+			timelabel.ForeColor = Color.Black; //установка чёрного цвета таймера
+			timelabel.Text = $"{timeleft} секунд.";
+			timer1.Start();
+		}
 
+		/// <summary>
+		/// метод обрабатывающий событие нажатия кнопки "начать тест"
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void ClickStart(object sender, EventArgs e)
+		{
+			words = new List<string>(ALLwords); //возврат всех слов в коллекцию
+			StartLT();
+			startButton.Enabled = false;
+			changeTypeButton.Enabled = false;
+		}
 
+		/// <summary>
+		/// обработчик тика таймера.
+		/// </summary>
+		/// <remarks>
+		/// каждую секунду проверяются ответы и оставшееся время.
+		/// </remarks>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void timer1_Tick(object sender, EventArgs e)
+		{
+			if (CheckAns()) //если все ответы верны:
+			{
+				timer1.Stop(); //останавливается таймер,
+
+				//добавлено не по заданию.
+				timelabel.Text = "гений нашёлся.."; //вывод сообщения в лейбл таймера о победе.
+
+				MessageBox.Show("наверняка калькулятор использовал да?", "ну че гений"); //выводится сообщение о завершении,
+				startButton.Enabled = true; //включается кнопки старта.
+				changeTypeButton.Enabled = true;
+			}
+			else if (timeleft > 0) //иначе если время ещё осталось:
+			{
+				timeleft--; //вычитается секунда из таймера,
+				timelabel.Text = $"{timeleft} секунд."; //вывод оставшегося времени.
+			}
+			else //если нет верных ответов и не осталось времени:
+			{
+				timer1.Stop(); //остановка таймера,
+				timelabel.Text = "лашпет!"; //вывод сообщения в label для таймера,
+				MessageBox.Show("за временем надо следить.", "не повезло"); //меседжбокс с сообщением об опоздании,
+
+				//установка для всех полей с ответами верные ответы
+				firstBox.Text = first;
+				secondBox.Text = second;
+				thirdBox.Text = third;
+				fourthBox.Text = fourth;
+				fifthBox.Text = fifth;
+
+				//включение кнопки старта.
+				startButton.Enabled = true;
+				changeTypeButton.Enabled = true;
+			}
+			if (timeleft <= 5) timelabel.ForeColor = Color.Red; //если осталось 5 секунд - установка красного цвета текста таймера.
+		}
 
 
 		//важные методы для формы.
@@ -117,5 +260,5 @@ namespace Math_Quiz
 		{
 			Application.Exit();
 		}
-	}
+    }
 }
